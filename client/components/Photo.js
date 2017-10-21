3dc534357e21fd62568a7b3c253821af23c9e57e
yann <yann.boisselier@gmail.com>
2017-10-21 19:02:57 +0200

    add like button

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -1,14 +1,41 @@
 import React from 'react'
 import { Link } from 'react-router'
+import CSSTransitionGroup from 'react-addons-css-transition-group'
 
 class Photo extends React.Component {
 
-  render() {
-    const {post} = this.props
+  render () {
+    const { post, comments } = this.props
     return (
       <figure className="grid-figure">
-        <img src={this.props.post.display_src} />
-        <Link to={`/view/${post.id}`}>{ this.props.post.caption }</Link>
+        <div className="grid-photo-wrap">
+          <Link to={`/view/${post.code}`}>
+            <img src={post.display_src} alt={post.caption} className='grid-photo' />
+          </Link>
+
+          <CSSTransitionGroup
+            transitionName="like"
+            transitionEnterTimeout={500}
+            transitionLeaveTimeout={500}
+          >
+            <span key={post.likes} className="likes-heart">
+              {post.likes}
+            </span>
+          </CSSTransitionGroup>
+
+          <figcaption>
+            <p>{post.caption}</p>
+            <div className="control-buttons">
+              <button className="likes">&hearts; {post.likes} </button>
+              <Link className="button" to={`/view/${post.code}`}>
+                <span className="comment-count">
+                  <span className="speech-bubble"></span>
+                  {comments[post.code] ? comments[post.code].length : '0'}
+                </span>
+              </Link>
+            </div>
+          </figcaption>
+        </div>
       </figure>
     )
   }
