14248f7fb32895c0c717a48bb24dc3cbd7a4f134
yann <yann.boisselier@gmail.com>
2017-10-22 00:32:37 +0200

    add photo detail page

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import Photo from './Photo'
 
 class Single extends React.Component {
 
   render() {
+    const postId = this.props.params.postId
+    const index = this.props.posts.findIndex(post => post.code === postId)
+    const post = this.props.posts[index]
     return (
       <div className="single-photo">
-      I am single photo
+        <Photo {...this.props} post={post} index={index} />
       </div>
     )
   }
